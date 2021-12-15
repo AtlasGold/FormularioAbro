8decf19a382c45f8440b78b4250973fe7a9bab2f
Luan Vinicius <luan.vini31@hotmail.com>
2021-12-15 14:44:40 -0300

    uhuul1

--- Controllers/CadastroControler.py
+++
@@ -1,9 +0,0 @@
-from models.cadastro import cadastro
-import services.database as db;
-
-def incluir(cadastro):
-    count = db.cursor.execute("""
-    INSERT INTO cadastro (nome, telefone, email) 
-    VALUES (?,?,?)""",
-    cadastro.nome, cadastro.telefone, cadastro.email).rowcount
-    db.cnxn.commit()
